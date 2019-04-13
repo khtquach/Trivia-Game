e7eb613f34d0ebab3e1f687c13bc691f4907cce3
Kim Helen Quach <KimHelenQuach@kims-air-2.attlocal.net>
2019-04-13 07:40:20 -0700

    second commit

---
+++ assets/Javscript/app.js
@@ -0,0 +1,23 @@
+
+var runGame
+    if (runGame === true) {
+        
+    }
+
+// $(document).ready()
+
+// var startbutton 
+//     //click function to load document//
+
+// var countDownTimer = Math.floor((distance % (1000*60)/1000));
+
+// var results
+
+// var 
+   
+// function myFunction() {
+    // document.body.style.backgroundImage ="url('https://images.wallpaperscraft.com/image/minimalism_geometric_landscape_124072_1920x1080.jpg')"
+// }
+
+
+
